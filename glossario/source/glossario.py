881a83fdeb6c9d7b5a1307f6988b07a2d0a7e18f
bazz333 <matteobazzan333@gmail.com>
2024-12-08 17:33:26 +0100

    add function replace_terms_in_file

--- glossario/source/glossario.py
+++ glossario/source/glossario.py
@@ -12,24 +12,26 @@ def glossario_terms():
                 gloss_terms.append(term)
     return gloss_terms
 
+def replace_terms_in_file(file_path, terms):
+    file_content = ""
+    with open(file_path, "r") as f:
+        file_content = f.read()
+        for term in terms:
+            if term in file_content and f"#gloss[ {term} ]" not in file_content:
+                print(f"Found '{term}' in {file_path}")
+                file_content = file_content.replace(term, f"#gloss[{term}]")
+    with open(file_path, "w") as f:
+        f.write(file_content)
+
 def search_files():
     gloss_terms = glossario_terms()
     skip_dirs = {"diari-di-bordo", "glossario", "1 - candidatura", "template"}
     for root, dirs, files in os.walk("./../../"):
+        print(f"Searching in {root}")
         dirs[:] = [d for d in dirs if d not in skip_dirs]
         for file in files:
             if file.endswith(".typ"):
-                print(f"Searching in {root, file}")
-
                 file_path = os.path.join(root, file)
-                file_content = ""
-                with open(file_path, "r") as f:
-                    file_content = f.read()
-                    for term in gloss_terms:
-                        if term in file_content and f"#gloss[ {term} ]" not in file_content:
-                            print(f"Found '{term}' in {file}")
-                            file_content = file_content.replace(term, f"#gloss[ {term} ]")
-                with open(file_path, "w") as f:
-                    f.write(file_content)
+                replace_terms_in_file(file_path, gloss_terms)
 
 search_files()
